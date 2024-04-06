e9d4bf3f55affe247ff8fac95e3ad15786f1e19c
James O'Donoghue <jameso@astra.com>
2024-04-05 21:39:35 -0700

    chore: fix auth route

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["(/games.*)", "/games/new", "(.*/teams.*)"],
+  publicRoutes: ["(/games.*)", "/games/new", "(.*/team.*)"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ["/no-auth-in-this-route"],
