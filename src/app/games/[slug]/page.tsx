2237e530f2d675e828a40b39c69745a3d249aeda
James O'Donoghue <jameso@astra.com>
2024-04-08 16:09:00 -0700

    chore: update test setup

--- src/app/games/[slug]/page.tsx
+++ src/app/games/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import ScoreCounter from "~/app/games/[slug]/score-counter";
 import { db } from "~/server/db";
 
-export default async function Game({ params }: { params: { slug: number } }) {
+export default async function Game({ params }: { params: { slug: string } }) {
   const response = await db.game.findFirst({
-    where: { id: params.slug },
+    where: { id: parseInt(params.slug) },
     include: { homeTeam: true, awayTeam: true },
   });
   if (!response) {
