a66481b4cfa401a500949486fb44898d89d4ae6f
James O'Donoghue <jameso@astra.com>
2024-04-12 12:23:54 -0700

    chore: update new game component to fetch data on the server

---
+++ src/app/games/new/new-game-form.tsx
@@ -0,0 +1,126 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "~/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { createGame } from "./_actions";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Loader2 } from "lucide-react";
+import { useFormCustom } from "~/hooks/useForm";
+import { type Team } from "@prisma/client";
+const formSchema = z.object({
+  homeTeamId: z.string(),
+  awayTeamId: z.string(),
+});
+export default function NewGameForm({ teams }: { teams: Team[] }) {
+  console.log(teams);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const createGameWithForm = createGame.bind(null, {
+    homeTeamId: form.getValues().homeTeamId,
+    awayTeamId: form.getValues().awayTeamId,
+  });
+  const { isPending, formAction, onSubmit } = useFormCustom(
+    createGameWithForm,
+    null,
+  );
+  return (
+    <div className="mx-auto max-w-xl p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>New Game</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="flex flex-col gap-6"
+              action={formAction}
+              onSubmit={onSubmit}
+            >
+              <FormField
+                control={form.control}
+                name="homeTeamId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Home Team</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a team" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {teams.map((item) => (
+                          <SelectItem key={item.id} value={item.id.toString()}>
+                            {item.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+              <FormField
+                control={form.control}
+                name="awayTeamId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Away Team</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a team" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {teams.map((item) => (
+                          <SelectItem key={item.id} value={item.id.toString()}>
+                            {item.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+              {!isPending ? (
+                <Button disabled={!form.formState.isValid} type="submit">
+                  Create
+                </Button>
+              ) : (
+                <Button disabled>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Creating...
+                </Button>
+              )}
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
