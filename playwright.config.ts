72e73ad2d91401c816caf09c4f450601a518a62c
James O'Donoghue <jamesodonoghue@jamess-laptop.lan>
2024-05-25 13:56:15 -0700

    chore: update pw config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     baseURL: process.env.PREVIEW_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
     video: "on",
   },
   /* Run your local dev server before starting the tests */
